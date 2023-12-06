e4689c2297154ebe842545032516f0302eb18b62
TheBlackOne <6339858+TheBlackOne@users.noreply.github.com>
2023-12-06 06:33:41 +0100

    Added solutions for day 6

---
+++ 2023/Day6_1.py
@@ -0,0 +1,31 @@
+input = """Time:      7  15   30
+Distance:  9  40  200"""
+
+#with open('input.txt') as f:
+#    input = f.read()
+
+races = []
+
+if __name__ == "__main__":
+    lines = input.splitlines()
+    times = lines[0].split(': ')[-1].split()
+    distances = lines[1].split(': ')[-1].split()
+<
+    for time, distance in zip(times, distances):
+        races.append((int(time), int(distance)))
+
+    result = 1
+
+    for time, distance in races:
+        num_wins = 0
+        for hold_time in range(time):
+            speed = hold_time
+            remaining_time = time - hold_time
+            race_distance = speed * remaining_time
+
+            if race_distance > distance:
+                num_wins += 1
+        print(f"Ways to win: {num_wins}")
+        result *= num_wins
+
+    print(f"Result: {result}")
---
+++ 2023/Day6_2.py
@@ -0,0 +1,32 @@
+from tqdm import tqdm
+
+input = """Time:      7  15   30
+Distance:  9  40  200"""
+
+#with open('input.txt') as f:
+#    input = f.read()
+
+if __name__ == "__main__":
+    lines = input.splitlines()
+    time = int(lines[0].split(': ')[-1].replace(' ', ''))
+    distance = int(lines[1].split(': ')[-1].replace(' ', ''))
+
+    result = 1
+
+    num_wins = 0
+    win_found = False
+
+    for hold_time in tqdm(range(time)):
+        speed = hold_time
+        remaining_time = time - hold_time
+        race_distance = speed * remaining_time
+
+        if race_distance > distance:
+            num_wins += 1
+            win_found = True
+        elif win_found:
+            break
+
+    result *= num_wins
+
+    print(f"Result: {result}")
