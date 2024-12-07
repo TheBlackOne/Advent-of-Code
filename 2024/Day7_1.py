320af61fe504657bf09c99a6aa6d0a28d6eb9d9e
TheBlackOne <6339858+TheBlackOne@users.noreply.github.com>
2024-12-07 09:13:45 +0100

    Day 7 solutions

---
+++ 2024/Day7_1.py
@@ -0,0 +1,50 @@
+from tqdm import tqdm
+
+input = """190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20"""
+
+# with open("input.txt") as f:
+#    input = f.read()
+
+valid_test_values = set()
+
+
+def process(operants, test_value):
+    global sum
+
+    if len(operants) > 1:
+        if operants[0] > test_value:
+            return
+
+        plus_result = operants[0] + operants[1]
+        mul_result = operants[0] * operants[1]
+
+        plus_list = operants[1:]
+        plus_list[0] = plus_result
+
+        mul_list = operants[1:]
+        mul_list[0] = mul_result
+
+        process(plus_list, test_value)
+        process(mul_list, test_value)
+    else:
+        if operants[0] == test_value:
+            valid_test_values.add(test_value)
+
+
+if __name__ == "__main__":
+    for line in input.splitlines():
+        test_value, operants = line.split(": ")
+        test_value = int(test_value)
+        operants = list(map(int, operants.split()))
+
+        process(operants, test_value)
+
+    print(sum(valid_test_values))
