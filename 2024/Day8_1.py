c6969cb3bd629f8835bfd6d9c26a4e0f5b73d156
TheBlackOne <6339858+TheBlackOne@users.noreply.github.com>
2024-12-08 07:06:45 +0100

    Day 8 solutions

---
+++ 2024/Day8_1.py
@@ -0,0 +1,53 @@
+import itertools
+
+input = """............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............"""
+
+# with open("input.txt") as f:
+#    input = f.read()
+
+
+if __name__ == "__main__":
+    antennas = {}
+
+    input = input.splitlines()
+    max_x = len(input[0])
+    max_y = len(input)
+
+    lim_x = range(max_x)
+    lim_y = range(max_y)
+
+    for y, line in enumerate(input):
+        x = 0
+        for x, c in enumerate(line):
+            if c != ".":
+                if c not in antennas.keys():
+                    antennas[c] = []
+                antennas[c].append((x, y))
+
+    antinode_coords = set()
+
+    for antenna_coords in antennas.values():
+        for c1, c2 in itertools.combinations(antenna_coords, 2):
+            # print(f"{c1} {c2}")
+            d = (c2[0] - c1[0], c2[1] - c1[1])
+            antinode = tuple(map(sum, zip(c2, d)))
+            if antinode[0] in lim_x and antinode[1] in lim_y:
+                antinode_coords.add(antinode)
+
+            d = tuple([c * -1 for c in d])
+            antinode = tuple(map(sum, zip(c1, d)))
+            if antinode[0] in lim_x and antinode[1] in lim_y:
+                antinode_coords.add(antinode)
+
+    print(len(antinode_coords))
