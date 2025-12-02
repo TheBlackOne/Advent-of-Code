040a823e6b2d39800135f8945f70f4b752f023af
Carsten Holbach <carsten.holbach@avalanchestudios.se>
2025-12-02 06:43:37 +0100

    Swapped textwrapped with itertools approach

--- 2025/Day2_2.py
+++ 2025/Day2_2.py
@@ -1,6 +1,6 @@
-import textwrap
 from tqdm import tqdm
 import functools
+import itertools
 
 input = """11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
 1698522-1698528,446443-446449,38593856-38593862,565653-565659,
@@ -12,7 +12,7 @@ input = """11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
 invalid_ids = []
 
 def chop_string(string, length):
-    result = textwrap.wrap(string, length)
+    result = [''.join(t) for t in itertools.batched(string, length)]
     return result
 
 # totally unnessary memoization flex
