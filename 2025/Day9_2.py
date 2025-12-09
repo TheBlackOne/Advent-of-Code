7e603ececf1bb6a88e7ef96aae0b1c78709679d4
Carsten Holbach <carsten.holbach@avalanchestudios.se>
2025-12-09 11:54:49 +0100

    Refactored part 2 so it works with line crosses

--- 2025/Day9_2.py
+++ 2025/Day9_2.py
@@ -1,6 +1,5 @@
 import itertools
 import shapely
-from shapely import Polygon
 import tqdm
 
 input = """7,1
@@ -13,7 +12,7 @@ input = """7,1
 7,3"""
 
 #with open("input.txt") as f:
-#   input = f.read()
+#    input = f.read()
 
 if __name__ == "__main__":
     red_tiles = []
@@ -23,26 +22,29 @@ if __name__ == "__main__":
         x, y = line.split(',')
         red_tiles.append((int(x), int(y)))
     
-    polygon = Polygon(red_tiles)
+    polygon = shapely.LinearRing(red_tiles)
     shapely.prepare(polygon)
 
     areas = []
-
-    for corner1, corner2 in tqdm.tqdm(list(itertools.combinations(red_tiles, 2))):
+    for corner1, corner2 in itertools.combinations(red_tiles, 2):
         if any(c1 == c2 for c1, c2 in zip(corner1, corner2)):
             continue
+        area = (abs(corner2[0] - corner1[0]) + 1) * (abs(corner2[1] - corner1[1]) + 1)
+        areas.append((area, corner1, corner2))
 
-        corners = [corner1, corner2]
-        corners.append((corner1[0], corner2[1]))
-        corners.append((corner2[0], corner1[1]))
-        corners = set(corners)
+    answer = None
+    for area, corner1, corner2 in tqdm.tqdm(sorted(areas, key=lambda x: x[0])[::-1]):
+        corner3 = (corner1[0], corner2[1])
+        corner4 = (corner2[0], corner1[1])
 
-        if len(corners) == 4:
-            test_polygon = Polygon(corners)
-            test_polygon = shapely.make_valid(test_polygon)
-            
-            if shapely.covers(polygon, test_polygon):
-                area = (abs(corner2[0] - corner1[0]) + 1) * (abs(corner2[1] - corner1[1]) + 1)
-                areas.append(area)
+        lines = []
+        lines.append(shapely.LineString((corner1, corner3)))
+        lines.append(shapely.LineString((corner1, corner4)))
+        lines.append(shapely.LineString((corner2, corner3)))
+        lines.append(shapely.LineString((corner3, corner4)))
 
-    print(max(areas))
+        if not any(shapely.crosses(polygon, l) for l in lines):
+            answer = area
+            break
+
+    print(answer)
